f13c4742683490d3ad0578c574c4ee31cc22d0cf
“enamul58” <enamullimon@gmail.com>
2024-03-10 22:12:12 +0600

    exercise01_1

--- chapter_1/exercise01_1.java
+++ chapter_1/exercise01_1.java
@@ -1,5 +1,5 @@
-public class exercise1_1 {
-    public static void main(String args[]){
+public class exercises1 {
+    public static void main(String args[]) {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
         System.out.println("Programming is fun");
