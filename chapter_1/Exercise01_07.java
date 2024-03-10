a5f7dc47565f7f53810e26262f3cc4dbf0f5620d
“enamul58” <enamullimon@gmail.com>
2024-03-10 23:32:45 +0600

    .

---
+++ chapter_1/Exercise01_07.java
@@ -0,0 +1,10 @@
+public class Exercise01_07 {
+    public static void main(String args[]){
+        double countOne = 4.0/1 + (-4.0/3) + 4.0/5 + (-4.0/7) + 4.0/9 + (-4.0/11);
+        double countTwo = 4.0/1 + (-4.0/3) + 4.0/5 + (-4.0/7) + 4.0/9 + (-4.0/11) + 4.0/13;
+
+        System.out.println(countOne);
+
+        System.out.println(countTwo);
+    }
+}
